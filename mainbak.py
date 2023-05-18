a0cd66e101ad0f24229b732c0b96e9b183382d7d
o85757o <541193491@qq.com>
2023-05-18 15:28:17 +0800

    在作者基础上将单个网页输出的md文档排版稍作改变

--- mainbak.py
+++ mainbak.py
@@ -56,7 +56,7 @@ def judge_zhihu_type(url):
 
     elif url.find("answer") != -1:
         # 如果是回答
-        parse_zhihu_answer2(url)
+        parse_zhihu_answer(url)
         
     else:
         # 如果是单篇文章
@@ -129,79 +129,6 @@ def save_and_transform(title_element, content_element, author, url):
         f.write(markdown)
 
 
-def save_and_transform2(title_element, content_element, author, url,
-                        support_element, edit_time_element, tags_element):
-    """
-    转化并保存为 Markdown 格式文件
-    """
-    # 获取标题和内容
-    if title_element is not None:
-        title = title_element.text.strip()
-    else:
-        title = "Untitled"
-    # 防止文件名称太长，加载不出图像
-    markdown_title = get_valid_filename(title)[0:20]
-    markdown_title = f"{markdown_title}_{author}"
-
-    if content_element is not None:
-        # 将 css 样式移除
-        for style_tag in content_element.find_all("style"):
-            style_tag.decompose()
-
-        for img_lazy in content_element.find_all("img", class_=lambda x: 'lazy' in x if x else True):
-            img_lazy.decompose()
-
-        # 处理回答中的图片
-        for img in content_element.find_all("img"):
-            # 将图片链接替换为本地路径
-            img_url = img.get("data-original", img.get("src", None))
-            if img_url is None:
-                continue
-
-            img_name = urllib.parse.quote(os.path.basename(img_url))
-            img_path = f"{markdown_title}_files/{img_name}"
-
-            # 如果图片链接中 .jpg 后面还有字符串则直接截停
-            if img_path.find('.jpg') + 3 != len(img_path) - 1:
-                img_path = img_path[0: img_path.find('.jpg') + 4]
-
-            img["src"] = img_path
-
-            # 下载图片并保存到本地
-            os.makedirs(os.path.dirname(img_path), exist_ok=True)
-            download_image(img_url, img_path)
-
-            # 在图片后插入换行符
-            img.insert_after('\n\n')
-
-        # 在 </figcaption> 后面加上换行符
-        for figcaption in content_element.find_all("figcaption"):
-            figcaption.insert_after('\n\n')
-
-        # 获取回答文本内容
-        content = content_element.decode_contents().strip()
-        # 转换为 markdown
-        content = md(content)
-    else:
-        content = ""
-
-    # 额外内容的解析
-    # 编辑日期
-    date = edit_time_element[4:20]
-    # 转化为 Markdown 格式
-    if content:
-        markdown = f"---\ntitle: {title}\ndate: {date}\ntags: {tags_element}\n" \
-                   f"categories: {tags_element}\n---\n\n\n" \
-                   f"# {title}\n\n**Author:** [{author}]\n{edit_time_element}\n" \
-                   f"{support_element}\n**Link:** [{url}]\n\n{content}"
-    else:
-        markdown = f"# {title}\n\nContent is empty."
-
-    # 保存 Markdown 文件
-    with open(f"{markdown_title}.md", "w", encoding="utf-8") as f:
-        f.write(markdown)
-
-
 def parse_zhihu_article(url):
     """
     解析知乎文章并保存为Markdown格式文件
@@ -240,26 +167,6 @@ def parse_zhihu_answer(url):
     save_and_transform(title_element, content_element, author, url)
 
 
-def parse_zhihu_answer2(url,categories="unkown"):
-    """
-    解析知乎回答并保存为 Markdown 格式文件
-    """
-    # 发送 GET 请求获取网页内容
-    response = requests.get(url)
-
-    # 解析 HTML
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    # 找到回答标题、内容、作者所在的元素
-    title_element = soup.select_one("h1.QuestionHeader-title")
-    content_element = soup.select_one("div.RichContent-inner")
-    author = soup.select_one('div.AuthorInfo > div.AuthorInfo-content > div.AuthorInfo-head').text.strip()
-    support_element = soup.select_one("div.ContentItem-actions > span").text.strip()
-    edit_time_element = soup.select_one("div.ContentItem-time").text.strip()
-    # 解析知乎文章并保存为Markdown格式文件
-    save_and_transform2(title_element, content_element, author, url, support_element, edit_time_element, categories)
-
-
 def parse_zhihu_column(url):
     """
     解析知乎专栏并获取所有文章链接
@@ -287,12 +194,12 @@ def parse_zhihu_column(url):
 if __name__=="__main__":
 
     # 回答
-    url = "https://www.zhihu.com/question/593914819/answer/2971671307"
+    # url = "https://www.zhihu.com/question/593914819/answer/2971671307"
 
     # 文章
     # url = "https://zhuanlan.zhihu.com/p/626703154"
 
     # 专栏
-    # url = "https://www.zhihu.com/column/c_1620937636624703488"
+    url = "https://www.zhihu.com/column/c_1620937636624703488"
 
     judge_zhihu_type(url)
